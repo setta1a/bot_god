4bf916cabc01e95d438e0ff031451e8b1a04ed8d
Andrey Sitalo <andreysitalo09@gmail.com>
2023-03-12 00:46:53 +0300

    do bot_create page

--- dj_project/settings.py
+++ dj_project/settings.py
@@ -119,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    'staticroot'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
