7b7b2e34ab940db9ea09b801c5093c76ef3bda0c
Simon Anikin <s.anikin1309@gmail.com>
2023-03-12 16:25:05 +0300

    Add bot_functions model

--- first/models.py
+++ first/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class BotFunctions(models.Model):
+    name = models.CharField(max_length=64)
+    code = models.CharField(max_length=1024)
