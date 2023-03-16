4dbd4df509e8aabb64b7413fd236e977a79e8e3a
Simon Anikin <s.anikin1309@gmail.com>
2023-03-16 19:42:21 +0300

    Change db for files

--- first/models.py
+++ first/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class BotFunctions(models.Model):
-    name = models.CharField(max_length=64)
-    code = models.CharField(max_length=1024)
+    func_name = models.CharField(max_length=64)
+    file_name = models.CharField(max_length=64)
