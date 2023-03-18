2efb15a985da51b2a17acd7154b7ebbeea90708a
Valeriy Vesenev <vesclop@yandex.ru>
2023-03-18 17:11:38 +0300

    create pdf to docx func

--- first/botTelegram/bot.py
+++ first/botTelegram/bot.py
@@ -20,9 +20,43 @@ bot = TeleBot(config["token"])
 
 #функции вставлять сюда
 
+def convert_pdf2docx(input_file: str, output_file: str, pages: Tuple = None):
+    """Преобразует PDF в DOCX"""
+    if pages:
+        pages = [int(i) for i in list(pages) if i.isnumeric()]
+    result = parse(pdf_file=input_file, docx_with_path=output_file, pages=pages)
+    summary = {
+        "Исходный файл": input_file,
+        "Страниц": str(pages),
+        "Результат преобразования": output_file
+    }
+    # Печать сводки
+    print("#### Отчет ########################################################")
+    print("\n".join("{}:{}".format(i, j) for i, j in summary.items()))
+    print("###################################################################")
+    return result
 
+@bot.message_handler(commands=['pdf2docx'])
+def pdf2docx_command(message):
+    os.mkdir("files")
+    send = bot.send_message(message.chat.id, 'Отправьте pdf файл')
+    bot.register_next_step_handler(send, pdf2docx)
 
+def pdf2docx(message):
+    file_info = bot.get_file(message.document.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
 
+    src = 'files/' + message.document.file_name
+    tmpdocx = open("files/tmpdocx.docx", "w")
+    tmpdocx.close()
+    with open(src, 'wb') as new_file:
+        new_file.write(downloaded_file)
+    convert_pdf2docx(src, "files/tmpdocx.docx")
+    srcdocx = src[0:len(src)-3]+'docx'
+    bot.send_document(message.chat.id, open(srcdocx, 'rb'))
+    os.remove("files/tmpdocx.docx")
+    os.remove(src)
+    os.remove(srcdocx)
 
 
 
