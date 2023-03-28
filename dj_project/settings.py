1544defe64f5fe74ded9ab21b670d38d655aa874
Andrew Sitalo <andreysitalo09@gmail.com>
2023-03-28 23:24:20 +0300

    delete operations

--- dj_project/settings.py
+++ dj_project/settings.py
@@ -90,8 +90,12 @@ WSGI_APPLICATION = 'dj_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
