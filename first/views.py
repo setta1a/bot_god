9fff2a58ea2a82bcbb17679c7193bbcfe670fffb
Alina Smolkina <alina_biolog05@mail.ru>
2023-04-03 17:25:49 +0300

    WIP beginning of the code

--- first/views.py
+++ first/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
-
+import json
+def create_file():
+    with open('example.json', 'r') as f:
+        data = json.loads(f.read())
+        for i in data['employees']['employee']:
+            if i['id'] == '3':
+                print(i['photo'])
 
 # Create your views here.
