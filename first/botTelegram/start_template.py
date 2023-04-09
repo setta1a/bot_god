61614d0283acac7d66e03fdfd053d54ccaf29566
Simon Anikin <s.anikin1309@gmail.com>
2023-04-09 17:20:39 +0300

    Fix little bugs

--- first/botTelegram/start_template.py
+++ first/botTelegram/start_template.py
@@ -2,16 +2,13 @@ import os
 import time
 from bs4 import BeautifulSoup
 import mouse as mouse
-import pyowm
 import requests
-import telebot
 import wikipedia
 from telebot import TeleBot
-from telebot import types
 from PIL import Image, ImageGrab, ImageDraw
 import platform
 import json
-with open('config.json') as json_file:
+with open('irst/botTelegram/config.json') as json_file:
     config = json.load(json_file)
 wikipedia.set_lang("ru")
 bot = TeleBot(config["token"])
