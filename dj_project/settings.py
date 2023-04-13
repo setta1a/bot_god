b909a17b1b0b7b91a1953e819d4192e0eb230b8b
Andrew Sitalo <andreysitalo09@gmail.com>
2023-04-13 13:01:06 +0300

    sd

--- dj_project/settings.py
+++ dj_project/settings.py
@@ -124,9 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/staticroot/"
-STATICFILES_DIRS = (
-    "staticroot"
-)
+STATICFILES_DIRS = "/staticroot/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
