2fbe6ce9b10d6d8de4c42c22932c41c952b1d1f1
Andrew Sitalo <andreysitalo09@gmail.com>
2023-04-16 21:51:30 +0300

    fix photos

--- dj_project/settings.py
+++ dj_project/settings.py
@@ -130,10 +130,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "staticroot/"
 STATICFILES_DIRS = [
-    'staticroot'
+    os.path.join(BASE_DIR, 'staticroot'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
