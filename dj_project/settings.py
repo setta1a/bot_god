612add2e27c97de55c4ee376551d6ec4f415f392
Andrew Sitalo <andreysitalo09@gmail.com>
2023-04-21 23:16:47 +0300

    sdasdA

--- dj_project/settings.py
+++ dj_project/settings.py
@@ -66,6 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'dj_project.urls'
