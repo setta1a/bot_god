36a31a50ef64714a7cffb4c916c7b4826a1f8b17
Andrew Sitalo <andreysitalo09@gmail.com>
2023-04-26 10:31:49 +0300

    db

--- dj_project/settings.py
+++ dj_project/settings.py
@@ -98,11 +98,13 @@ WSGI_APPLICATION = 'dj_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "OPTIONS": {
-            "read_default_file": "../my.cnf",
-        },
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bot_god_db',
+        'USER': 'vasya',
+        'PASSWORD': 'promprog',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
