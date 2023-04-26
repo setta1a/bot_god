fabff09f10c436a639f7cb446e81bac519e330b6
Andrew Sitalo <andreysitalo09@gmail.com>
2023-04-26 10:05:47 +0300

    change db

--- dj_project/settings.py
+++ dj_project/settings.py
@@ -98,9 +98,11 @@ WSGI_APPLICATION = 'dj_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "OPTIONS": {
+            "read_default_file": "../my.cnf",
+        },
     }
 }
 
