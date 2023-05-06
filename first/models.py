04c572648f38d5086a750a5f9827346825c0ab55
Andrew Sitalo <andreysitalo09@gmail.com>
2023-05-06 19:58:23 +0300

    sda

--- first/models.py
+++ first/models.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import User
 from django.db import models
 # Create your models here.
+
 class BotFunctions(models.Model):
     func_name = models.CharField(max_length=64)
     file_name = models.CharField(max_length=64)
+
 
 class BotPreSets(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
@@ -11,6 +13,7 @@ class BotPreSets(models.Model):
     created_at = models.DateTimeField()
     token = models.CharField(max_length=64)
     os = models.CharField(max_length=10)
+
 
 class FuncForPresets(models.Model):
     func = models.ForeignKey(to=BotFunctions, on_delete=models.CASCADE)
