ce17b18a99a2be87a652efd2506eb5e0e677f915
Andrew Sitalo <andreysitalo09@gmail.com>
2023-05-20 20:47:51 +0300

    Empty comment

--- dj_project/tasks.py
+++ dj_project/tasks.py
@@ -9,7 +9,6 @@ from .celerys import app
 
 @app.task
 def generate_bot(short_name: str, function_names: list, file_names: list, token: str, bot_os: str, user_name: str):
-    os.chdir("")
     file_dir = os.getcwd() + "/staticroot/BOT"
     if os.path.exists(file_dir):
         shutil.rmtree(file_dir)
