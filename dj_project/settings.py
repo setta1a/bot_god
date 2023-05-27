6efa15b8dc6026f5b7f748dfa6dd30b039782ac0
Andrew Sitalo <andreysitalo09@gmail.com>
2023-05-27 20:01:01 +0300

    Empty comment

--- dj_project/settings.py
+++ dj_project/settings.py
@@ -150,7 +150,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticroot'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-q
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
